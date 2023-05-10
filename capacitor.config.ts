332547ef52f2d373f82cc055dc258cc2d22f8e96
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-05-09 22:31:00 -0500

    f

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+
+  plugins:{
+    GoogleAuth:{
+      scopes:['profile', 'email'],
+      serviceClientId:"71405197754-7kjjq4inpbs2hao5aknp3u1on6tcosru.apps.googleusercontent.com",
+      forceCodeForRefreshToken:true,
+    }
+
+  },
   appId: 'io.ionic.starter',
   appName: 'SDNotes',
   webDir: 'www',
